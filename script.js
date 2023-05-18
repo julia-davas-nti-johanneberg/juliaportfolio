3afe854573209c6d86a35bcf086a81cebd4ec3da
julia-davas-nti-johanneberg <94617563+julia-davas-nti-johanneberg@users.noreply.github.com>
2023-05-18 15:58:09 +0200

    18may
    filer, mappar, småfix

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+// hämtar html-taggar
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+// taggarna togglas
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+}
+
+// lyssnar när någon klickar på hamburgarmenyn
+burger.addEventListener('click', toggleMeny)
 
 
 
 
 
 
-// let card = document.querySelector(".card");
 
 
-// function cardFlipper() {flipcar
-//     card.classList.toggle('d')
-//     console.log('flip flip flip')
-// }
+myID = document.getElementById("myID");
 
-// card.addEventListener('click', cardFlipper)
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 800) {
+        myID.className = "bottomMenu show"
+    } else {
+        myID.className = "bottomMenu hide"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
